fab3189cc42bec45f83d1ab02199f3ef4d122eb4
shengjk1 <shengzi789>
2017-02-13 13:52:32 +0800

    111111111111111111111111111

--- src/main/java/xmht/sjms/createModel/factory/multipleFactory/SendFactory.java
+++ src/main/java/xmht/sjms/createModel/factory/multipleFactory/SendFactory.java
@@ -19,4 +19,21 @@ public class SendFactory {
 	public Sender produceSms(){
 		return new SmsSender();
 	}
+	
+	/**
+	 * Created by shengjk1 on 2017/1/6.
+	 */
+	/*
+	多个工厂方法模式
+	 */
+	public static class SendFactory {
+		public Sender produceMail(){
+			return new MailSender();
+		}
+		
+		
+		public Sender produceSms(){
+			return new SmsSender();
+		}
+	}
 }
