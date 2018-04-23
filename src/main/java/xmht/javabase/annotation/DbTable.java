f3c54327908ccc4a2e8c0d6004d24f4b098be9a6
shengjk <jsjsjs1789@163.com>
2018-04-23 10:56:38 +0800

    java8Demo

--- src/main/java/xmht/javabase/annotation/DbTable.java
+++ src/main/java/xmht/javabase/annotation/DbTable.java
@@ -70,45 +70,6 @@ class Member {
 	static int memberCount;
 	
 	
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	public String getLasttName() {
-		return lasttName;
-	}
-	
-	public void setLasttName(String lasttName) {
-		this.lasttName = lasttName;
-	}
-	
-	public Integer getAge() {
-		return age;
-	}
-	
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-	
-	public String getHandle() {
-		return handle;
-	}
-	
-	public void setHandle(String handle) {
-		this.handle = handle;
-	}
-	
-	public static int getMemberCount() {
-		return memberCount;
-	}
-	
-	public static void setMemberCount(int memberCount) {
-		Member.memberCount = memberCount;
-	}
 }
 
 
